f6298ea80d6ca34ab4985bca4e8eb85fad99be4a
Florian Contival <>
2018-11-28 20:27:11 -0500

    Update start_mission.py

---
+++ start_mission.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import sys
+import argparse
+import dronekit
+import pymavlink
+
+
+def main(args):
+    ap = argparse.ArgumentParser()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
